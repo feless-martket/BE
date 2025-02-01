4f047752515849d1d338843cc6b56f55a6659a78
홍혁진 <gurwls123164@naver.com>
2025-02-01 17:00:06 +0900

    develop weekendSale sort

--- src/main/java/org/example/be/domain/Member.java
+++ src/main/java/org/example/be/domain/Member.java
@@ -18,6 +18,7 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.ToString;
 import lombok.experimental.FieldDefaults;
 
 
--- src/main/java/org/example/be/repository/CartRepository.java
+++ src/main/java/org/example/be/repository/CartRepository.java
@@ -1,9 +1,12 @@
 package org.example.be.repository;
 
 import org.example.be.domain.Cart;
+import org.example.be.domain.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CartRepository extends JpaRepository<Cart, Long> {
     Cart findByMember_Username(String username);
+    Cart findByMember(Member member);
 
+    void deleteCartById(Long id);
 }
