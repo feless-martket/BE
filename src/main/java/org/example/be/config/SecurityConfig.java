541b0fbb065da4b1aebc7ad8d20d7418d97c5d98
yoonhyojin <dbsgywls9855@gmail.com>
2025-02-05 05:40:03 +0900

    CICD TEST 48

--- src/main/java/org/example/be/config/SecurityConfig.java
+++ src/main/java/org/example/be/config/SecurityConfig.java
@@ -105,7 +105,7 @@ public class SecurityConfig {
         CorsConfiguration corsConfiguration = new CorsConfiguration();
 
         // 허용할 Origin 설정 (여러 개의 Origin 허용)
-        corsConfiguration.setAllowedOrigins(Arrays.asList("http://localhost:3000", "http://localhost:3001"));
+        corsConfiguration.setAllowedOrigins(Arrays.asList("http://localhost:3000", "http://localhost:3001", "http://3.34.181.33"));
 
         // 허용할 HTTP 메서드
         corsConfiguration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
