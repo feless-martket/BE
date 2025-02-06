380c237eacd7eb603ed44b5dabc05e5ba402e3e6
yoonhyojin <dbsgywls9855@gmail.com>
2025-02-06 17:17:29 +0900

    ec2 IP 수정 3

--- src/main/java/org/example/be/config/SecurityConfig.java
+++ src/main/java/org/example/be/config/SecurityConfig.java
@@ -105,7 +105,7 @@ public class SecurityConfig {
         CorsConfiguration corsConfiguration = new CorsConfiguration();
 
         // 허용할 Origin 설정 (여러 개의 Origin 허용)
-        corsConfiguration.setAllowedOrigins(Arrays.asList("http://localhost:3000", "http://localhost:3001", "http://3.36.171.210"));
+        corsConfiguration.setAllowedOrigins(Arrays.asList("http://localhost:3000", "http://localhost:3001", "http://3.36.171.210", "https://fe-seven-weld.vercel.app"));
 
         // 허용할 HTTP 메서드
         corsConfiguration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
